e737b35b9bd9ae551d3393e3b4c858714f6e16fc
SealOnARock <parthhivashley@gmail.com>
2023-09-05 07:25:05 +0530

    FIxed db.py

--- func/db.py
+++ func/db.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import mysql.connector
 
 def dbgen():
@@ -12,18 +11,3 @@ def dbgen():
 		print("Error occured! Unable to connect to database")
 
 	return db
-=======
-import mysql.connector
-
-def dbgen():
-	try:
-		db = mysql.connector.connect(
-			host ="localhost",
-			user ="passwdDB",
-			passwd ="qwerty"
-		)
-	except Exception as ex:
-		print("Error occured! Unable to connect to database")
-
-return db
->>>>>>> 3ccfe46e8f9938ae96700fa9b1023ca0618db014
